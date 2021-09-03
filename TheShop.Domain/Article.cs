a8ed8907801176e2e571ff92e22e1866e79d9b79
Roman Lazoryk <rlazoryk@intelvision.co>
2021-09-03 16:07:41 +0300

    Divide solution on different Domain, Repository and Service projects.

---
+++ TheShop.Domain/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TheShop.Domain
+{
+	public class Article
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int Price { get; set; }
+		public bool IsSold { get; set; }
+
+		public DateTime SoldDate { get; set; }
+		public int BuyerUserId { get; set; }
+	}
+}
---
+++ TheShop.Services/Suppliers/SupplierService3.cs
@@ -0,0 +1,23 @@
+﻿using TheShop.Domain;
+using TheShop.Services.Interfaces;
+
+namespace TheShop.Services.Suppliers
+{
+	public class SupplierService3 : ISupplierService
+	{
+		public bool ArticleInInventory(int id)
+		{
+			return true;
+		}
+
+		public Article GetArticle(int id)
+		{
+			return new Article()
+			{
+				Id = 1,
+				Name = "Article from supplier3",
+				Price = 460
+			};
+		}
+	}
+}
